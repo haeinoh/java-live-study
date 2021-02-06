3a5c4b0bd02af3fd4b0d69374fa8991956a270c3
HiMac <himac@HiMacui-MacBookAir.local>
2021-02-06 18:55:11 +0900

    week9 start

---
+++ week9/src/ExceptionHandling/TryCatch.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+public class TryCatch {
+    public static void main(String[] args) {
+
+//        try {
+//            //  예외 발생에 대비한 코드를 작성한다.
+//        } catch(Exception1 e1) {
+//            //  Exception1이 발생하였을 경우, 처리하는 코드를 작성한다.
+//        } catch(Exception2 e2) {
+//            // Exception2가 발생하였을 경우, 처리하는 코드를 작성한다.
+//        }
+
+    }
+}
